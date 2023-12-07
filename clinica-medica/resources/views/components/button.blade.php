421977f615e60912458e3603e0f34eeea9f40911
peluci <joaovalenteofficial@gmail.com>
2023-12-07 15:34:40 -0300

    feat: reestilizando a tela de login

--- clinica-medica/resources/views/components/button.blade.php
+++ clinica-medica/resources/views/components/button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-white hover:text-[#038E83] focus:bg-white active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-[#038e83] border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-[#4abfa1] focus:bg-white active:bg-[#4abfa1] focus:outline-none focus:ring-2 focus:ring-[#4abfa1] focus:ring-offset-2 transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </button>
--- clinica-medica/resources/views/components/checkbox.blade.php
+++ clinica-medica/resources/views/components/checkbox.blade.php
@@ -1,1 +1,1 @@
-<input type="checkbox" {!! $attributes->merge(['class' => 'rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500']) !!}>
+<input type="checkbox" {!! $attributes->merge(['class' => 'rounded border-[#4abfa1] text-[#4abfa1] shadow-sm focus:ring-[#4abfa1]']) !!}>
